75c3a59c6c126470af40cd28b0ff285515ceba1d
Chris Searle <chris@chrissearle.org>
2018-11-09 11:46:09 +0100

    Fixes #3. Last 10 posts

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,17 +1,56 @@
 import React from 'react'
 
+import { StaticQuery, graphql, Link } from 'gatsby'
+
 import { Card, CardHeader, ListGroup, ListGroupItem } from 'reactstrap'
 
-const RecentPosts = () => {
+const RecentPosts = ({ data }) => {
+  const { allMarkdownRemark } = data
+  const { edges } = allMarkdownRemark
+
   return (
     <Card className="mb-4">
       <CardHeader>Recent Articles</CardHeader>
       <ListGroup>
-        <ListGroupItem>1</ListGroupItem>
-        <ListGroupItem>2</ListGroupItem>
+        {edges.map(edge => {
+          const { frontmatter, fields } = edge.node
+
+          return (
+            <ListGroupItem key={fields.path}>
+              <Link to={fields.path}>{frontmatter.title}</Link>
+            </ListGroupItem>
+          )
+        })}
       </ListGroup>
     </Card>
   )
 }
 
-export default RecentPosts
+const WrappedRecentPosts = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          allMarkdownRemark(
+            sort: { order: DESC, fields: [frontmatter___date] }
+            filter: { fields: { collection: { eq: "blog" } } }
+            limit: 10
+          ) {
+            edges {
+              node {
+                fields {
+                  path
+                }
+                frontmatter {
+                  title
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => <RecentPosts data={data} />}
+    />
+  )
+}
+export default WrappedRecentPosts
