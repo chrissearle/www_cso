1fee021526d5c753730611214926de08eec94fee
Chris Searle <chris@chrissearle.org>
2018-11-09 11:34:40 +0100

    Fixes #4. Top 10 tags

--- src/components/topTags.js
+++ src/components/topTags.js
@@ -1,17 +1,75 @@
 import React from 'react'
 
-import { Card, CardHeader, ListGroup, ListGroupItem } from 'reactstrap'
+import { StaticQuery, graphql, Link } from 'gatsby'
 
-const TopTags = () => {
+import { Card, CardHeader, ListGroup, ListGroupItem, Badge } from 'reactstrap'
+
+const TopTags = ({ data }) => {
+  const { allMarkdownRemark } = data
+  const { edges } = allMarkdownRemark
+
+  const postCountByTag = {}
+
+  edges.forEach(({ node }) => {
+    if (node.frontmatter.tags) {
+      const tags = node.frontmatter.tags.split(/, */)
+
+      tags.forEach(tag => {
+        if (!postCountByTag[tag]) {
+          postCountByTag[tag] = 0
+        }
+
+        postCountByTag[tag] += 1
+      })
+    }
+  })
+
+  const tags = Object.keys(postCountByTag)
+    .sort((a, b) => {
+      return postCountByTag[b] - postCountByTag[a]
+    })
+    .slice(0, 10)
+
   return (
     <Card className="mb-4">
       <CardHeader>Popular Tags</CardHeader>
       <ListGroup>
-        <ListGroupItem>1</ListGroupItem>
-        <ListGroupItem>2</ListGroupItem>
+        {tags.map(tag => {
+          return (
+            <ListGroupItem key={tag}>
+              <Link to={`/tags/${tag}`}>{tag}</Link>
+              <Badge pill color="dark" className="float-right">
+                {postCountByTag[tag]}
+              </Badge>
+            </ListGroupItem>
+          )
+        })}
       </ListGroup>
     </Card>
   )
 }
 
-export default TopTags
+const WrappedTopTags = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          allMarkdownRemark(
+            filter: { fields: { collection: { eq: "blog" } } }
+          ) {
+            edges {
+              node {
+                frontmatter {
+                  tags
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => <TopTags data={data} />}
+    />
+  )
+}
+
+export default WrappedTopTags
