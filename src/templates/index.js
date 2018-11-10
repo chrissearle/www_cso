97758de2b9817fd76c289b2b1945bf5ccba27648
Chris Searle <chris@chrissearle.org>
2018-11-10 15:35:54 +0100

    Refactor pagination out to own file

--- src/templates/index.js
+++ src/templates/index.js
@@ -4,8 +4,6 @@ import Layout from '../components/layout'
 
 import { Link } from 'gatsby'
 
-import _ from 'lodash'
-
 import {
   Card,
   CardBody,
@@ -15,22 +13,15 @@ import {
   CardColumns,
   CardFooter,
   CardImg,
-  Pagination,
-  PaginationItem,
-  PaginationLink,
 } from 'reactstrap'
 
 import moment from 'moment'
 
 import TagsMap from '../components/tagsMap'
+import Pagination from '../components/pagination'
 
 const Index = ({ pageContext }) => {
   const { group, index, pageCount } = pageContext
-  const previousUrl = index - 1 === 1 ? '/' : (index - 1).toString()
-  const nextUrl = (index + 1).toString()
-  console.log(previousUrl)
-
-  const pages = _.range(2, pageCount + 1)
 
   return (
     <Layout>
@@ -80,24 +71,7 @@ const Index = ({ pageContext }) => {
           )
         })}
       </CardColumns>
-      <Pagination listClassName="justify-content-center">
-        <PaginationItem disabled={index <= 1}>
-          <PaginationLink href={`/${previousUrl}`}>&lt;</PaginationLink>
-        </PaginationItem>
-        <PaginationItem active={1 === index}>
-          <PaginationLink href={'/'}>1</PaginationLink>
-        </PaginationItem>
-        {pages.map(page => {
-          return (
-            <PaginationItem active={page === index} key={`page_${page}`}>
-              <PaginationLink href={`/${page}`}>{page}</PaginationLink>
-            </PaginationItem>
-          )
-        })}
-        <PaginationItem disabled={index >= pageCount}>
-          <PaginationLink href={`/${nextUrl}`}>&gt;</PaginationLink>
-        </PaginationItem>
-      </Pagination>
+      <Pagination index={index} pageCount={pageCount} />
     </Layout>
   )
 }
