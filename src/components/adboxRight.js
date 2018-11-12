564d3f8e54c2d6bc0ce018051f292efc71d4e3c7
Chris Searle <chris@chrissearle.org>
2018-11-12 13:06:09 +0100

    Old ad size no longer available

--- src/components/adboxRight.js
+++ src/components/adboxRight.js
@@ -6,8 +6,8 @@ const AdBoxRight = () => {
   return (
     <AdSense.Google
       client="ca-pub-6208435820744910"
-      slot="5194006361"
-      style={{ display: 'inline-block', width: '160px', height: '90px' }}
+      slot="1662649506"
+      style={{ display: 'inline-block', width: '120px', height: '240px' }}
     />
   )
 }
