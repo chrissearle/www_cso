208262e1a43ebd6ad049bd919f7a16ec5db9e732
Chris Searle <chris@chrissearle.org>
2026-01-29 11:46:48 +0100

    Popover image

--- app/components/content/ProseImg.vue
+++ app/components/content/ProseImg.vue
@@ -1,19 +1,18 @@
 <template>
   <figure class="my-5">
-    <a
-      :href="refinedSrc"
-      target="_blank"
-      rel="noopener"
-      :title="alt"
+    <button
+      type="button"
+      class="w-full cursor-zoom-in focus:outline-none rounded-lg transition-all hover:shadow-lg hover:scale-[1.02]"
       :aria-labelledby="captionId"
+      @click="openLightbox"
     >
       <NuxtImg
         :src="refinedSrc"
         :alt="alt"
-        class="w-full max-h-[600px] object-contain"
+        class="w-full max-h-[600px] object-contain rounded-lg hover:opacity-90 transition-opacity"
       />
-      <span class="sr-only">{{ alt }}</span>
-    </a>
+      <span class="sr-only">Click to view full size: {{ alt }}</span>
+    </button>
 
     <figcaption
       :id="captionId"
@@ -22,37 +21,53 @@
       {{ alt }}
     </figcaption>
   </figure>
+
+  <ImageLightbox
+    :src="refinedSrc"
+    :alt="alt"
+    :is-open="isLightboxOpen"
+    @close="closeLightbox"
+  />
 </template>
 
 <script setup lang="ts">
-import { withBase } from "ufo"
-import { computed, useRuntimeConfig } from "#imports"
+import { withBase } from 'ufo'
+import { computed, useRuntimeConfig } from '#imports'
 
 const captionId = useId()
+const isLightboxOpen = ref(false)
 
 const props = defineProps({
   src: {
     type: String,
-    default: "",
+    default: ''
   },
   alt: {
     type: String,
-    default: "",
+    default: ''
   },
   width: {
     type: [String, Number],
-    default: undefined,
+    default: undefined
   },
   height: {
     type: [String, Number],
-    default: undefined,
-  },
+    default: undefined
+  }
 })
 
 const refinedSrc = computed(() => {
-  if (props.src?.startsWith("/") && !props.src.startsWith("//")) {
+  if (props.src?.startsWith('/') && !props.src.startsWith('//')) {
     return withBase(props.src, useRuntimeConfig().app.baseURL)
   }
   return props.src
 })
+
+const openLightbox = () => {
+  isLightboxOpen.value = true
+}
+
+const closeLightbox = () => {
+  isLightboxOpen.value = false
+}
 </script>
